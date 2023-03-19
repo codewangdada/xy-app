62c85be737c22e9d729ff5a5d95399ab45eab4a4
wangdada <codewyh@qq.com>
2023-03-19 14:20:13 +0800

    新增商品详情页

---
+++ src/pages/detail/index.vue
@@ -0,0 +1,247 @@
+<template>
+	<view class="page-container">
+		<view class="main-wrapper">
+			<view class="topbar-wrapper">
+				<image class="icon" src="../../static/icon/ic_back.png" @click="goBack"></image>
+				<view class="topbar-search">
+					请搜索你要的宝贝
+				</view>
+				<image class="icon topbar-share" src="../../static/icon/ic_share.png"></image>
+				<image class="icon" src="../../static/icon/ic_more.png"></image>
+			</view>
+			<view class="footer-wrapper">
+				<view class="footer-icon">
+					<image class="icon-small" src="../../static/icon/ic_bid.png"></image>
+					<view>
+						出价
+					</view>
+				</view>
+				<view class="footer-icon">
+					<image class="icon-small" src="../../static/icon/ic_leave.png"></image>
+					<view>
+						留言
+					</view>
+				</view>
+				<view class="footer-icon">
+					<image class="icon-small" src="../../static/icon/ic_collect.png"></image>
+					<view>
+						10
+					</view>
+				</view>
+				<view class="footer-btn btn">
+					卖同款
+				</view>
+				<view class="footer-btn">
+					我想要
+				</view>
+			</view>
+			<view class="userinfo-wrapper">
+				<image src="../../static/logo.png" class="userinfo-avatar"></image>
+				<view class="userinfo-box">
+					<view class="userinfo-name">
+						<text>小董</text><text class="tag">百次好评</text>
+					</view>
+					<view class="userinfo-time">
+						<text>2分钟前来过</text>|<text>济南</text>
+					</view>
+				</view>
+				<view class="userinfo-attention">
+					+ 关注
+				</view>
+			</view>
+			<view class="price-wrapper">
+				<view class="price">
+					￥
+				</view>
+				<view class="price-new">
+					36.98
+				</view>
+				<view class="small-text">
+					￥100
+				</view>
+				<view class="middle-line">
+					|
+				</view>
+				<view class="express">
+					包邮
+				</view>
+				<view class="small-text">
+					179人想要
+				</view>
+				<view class="middle-line">
+					|
+				</view>
+				<view class="small-text">
+					581浏览
+				</view>
+			</view>
+			<view class="content-wrapper">
+				15分钟内到账<br>
+				优酷vip黄金会员一年优酷youku视频会员12个月直冲<br>
+				充值到账12个月。15分钟内到账<br>
+				直冲您的账号，拍下后提供手机号给客服，需要配合接验证码，一次性到账，正规充值，<br>
+				1.直冲一次性到账，非共享账号，充值您的手机号<br>
+				2.支持手机，平板，电脑通用，充值速度快<br>
+				3.充值成功后，无法退款，请确认好充值账号，有问题可以私聊客服<br>
+			</view>
+			<view class="line">
+				
+			</view>
+		</view>
+	</view>
+</template>
+
+<script setup lang="ts">
+	function goBack() {
+		uni.navigateBack({
+			delta: 1
+		});
+	}
+</script>
+
+<style lang="scss" scoped>
+	.icon{
+		width: 50rpx;
+		height: 50rpx;
+	}
+	.icon-small{
+		width: 40rpx;
+		height: 40rpx;
+	}
+	.page-container {
+		padding-top: 80px;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+
+		.main-wrapper {
+			width: 100%;
+			position: relative;
+			.topbar-wrapper{
+				width: 100%;
+				height: 80px;
+				box-sizing: border-box;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				position: fixed;
+				top: 0;
+				left: 0;
+				padding: 80rpx 20rpx 10rpx;
+				.topbar-search{
+					flex: 1;
+					background-color: #eee;
+					color: #999;
+					margin: 0 20rpx;
+					padding: 10rpx 20rpx;
+					border-radius: 30rpx;
+				}
+				.topbar-share{
+					margin-right: 20rpx;
+				}
+			}
+			.footer-wrapper{
+				position: fixed;
+				width: 100%;
+				bottom: 0;
+				left: 0;
+				border-top: 1px solid #eee;
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+				padding: 20rpx 40rpx;
+				box-sizing: border-box;
+				.footer-icon{
+					text-align: center;
+					color: #999;
+					font-size: 20rpx;
+					margin-right: 40rpx;
+				}
+				.footer-btn{
+					background-color: rgb(255, 231, 17);
+					border-radius: 60rpx;
+					padding: 20rpx 40rpx;
+				}
+				.btn{
+					margin-left: auto;
+					margin-right: 20rpx;
+					background-color: #eee;
+				}
+			}
+			.userinfo-wrapper{
+				padding: 40rpx;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				.userinfo-avatar{
+					width: 80rpx;
+					height: 80rpx;
+					border-radius: 40rpx;
+				}
+				.userinfo-box{
+					margin-left: 20rpx;
+					flex: 1;
+					.userinfo-name{
+						font-weight: bold;
+						font-size: 28rpx;
+						.tag{
+							color: orange;
+							font-size: 24rpx;
+							margin-left: 10rpx;
+							background-color: yellow;
+							padding: 2rpx 10rpx;
+							border-radius: 20rpx;
+						}
+					}
+					.userinfo-time{
+						color: #666;
+						margin-top: 10rpx;
+						font-size: 24rpx;
+					}
+				}
+				.userinfo-attention{
+					font-weight: bold;
+					background-color: #eee;
+					border-radius: 50rpx;
+					padding: 10rpx 20rpx;
+				}
+			}
+			.price-wrapper{
+				display: flex;
+				align-items: baseline;
+				padding: 40rpx;
+				.price{
+					color: #ff5500
+				}
+				.price-new{
+					font-size: 40rpx;
+					font-weight: bold;
+					color: #ff5500
+				}
+				.small-text{
+					color: #666;
+					font-size: 24rpx;
+					
+				}
+				.express{ 
+					color: royalblue;
+					font-size: 28rpx;
+					flex: 1;
+				}
+				.middle-line{
+					color: #eee;
+					font-size: 24rpx;
+					margin: 0 10rpx;
+				}
+			}
+			.content-wrapper{
+				padding: 40rpx;
+			}
+			.line{
+				width: 100%;
+				height: 20rpx;
+				background-color: #eee;
+			}
+		}
+	}
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,43 +1,65 @@
 <template>
-	<view class="box">
-		<view class="header">
-			<view class="header-search">
-				<image class="header-search__icon" src="../../static/image/search.png"></image>
-				<view class="header-search__line">
+	<view class="page-container">
+		<view class="main-wrapper">
+			<view class="topbar-wrapper">
+				<view class="header-search">
+					<image class="header-search__icon" src="../../static/image/search.png"></image>
+					<view class="header-search__line">
 
+					</view>
+					<view class="header-search__text">
+						{{topH}}
+					</view>
+					<view class="header-search__btn">搜索</view>
 				</view>
-				<view class="header-search__text">
-					11111
+			</view>
+			<view class="gonge-wrapper">
+
+			</view>
+			<view class="category-wrapper">
+				<view class="category-item" @click="goTop">
+					猜你喜欢
 				</view>
-				<view class="header-search__btn">搜索</view>
+				<view class="category-item">
+					最新发布
+				</view>
+			</view>
+			<view class="recommend-container">
+				<view class="recommend-item" v-for="item in 7" @click="goDetail">
+					<a class="recommend-img-wrapper">
+						<image class="recommend-img" src="../../static/logo.png"></image>
+					</a>
+					<a class="recommend-info">
+						<view class="recommend-title">
+							<text class="recommend-title-p">
+								好侍咖王咖喱咖哩咖喱块中辣3号90ghouse块状咖喱调味料调料
+							</text>
+						</view>
+						<view class="recommend-price-box">
+							<text class="recommend-sign">
+								￥
+							</text>
+							<text class="recommend-price">
+								13.8
+							</text>
+							<text class="recommend-payed">
+								180人想要
+							</text>
+						</view>
+						<view class="recommend-person-box">
+							<image src="../../static/logo.png" class="recommend-avatar"></image>
+							<text class="recommend-address">
+								杭州
+							</text>
+							<text class="recommend-credit">
+								芝麻信用极好
+							</text>
+						</view>
+					</a>
+				</view>
 			</view>
 		</view>
-		<scroll-view class="content" scroll-y="true">
-			<view class="activity">
-				<view class="activity__wrap">
-					<view class="activity__item">
-						
-					</view>
-				</view>
-				<view class="activity__wrap">
-					<view class="activity__item">
-						
-					</view>
-				</view>
-			</view>
-			<view class="type">
-				1111111
-			</view>
-			<view class="container">
-				<view class="item-wrapper" v-for="(item, i) in 7">
-					<image src="../../static/logo.png" class="item" />
-					11111
-				</view>
-			</view>
-		</scroll-view>
-
 	</view>
-
 </template>
 
 <script setup lang="ts">
@@ -53,15 +75,44 @@
 	import {
 		useCounterStore
 	} from '@/stores/counter'
-
+	import {
+		onReady
+	} from "@dcloudio/uni-app";
+	
 	const listData = ref < IUserInfo[] > ([])
 	const count = ref < number > (0)
 	const counter = useCounterStore()
-
+	
+	const topH = ref < number > (0)
+	
+	onReady(() => {
+		uni.getSystemInfo({
+			success(res) {
+				let titleH = uni.createSelectorQuery().select(".category-item")
+				titleH.boundingClientRect(data => {
+					topH.value = data.top - 120 - 20
+				}).exec()
+			}
+		})
+	})
+	
+	function goTop() {
+		uni.pageScrollTo({
+			scrollTop:topH.value,
+			duration:100
+		})
+	}
+	
+	function goDetail() {
+		uni.navigateTo({
+			url:'/pages/detail/index'
+		})
+	}
+	
 	function add() {
 		counter.increment();
 	}
-
+	
 	getList();
 	async function getList() {
 		const {
@@ -76,112 +127,196 @@
 		} = data
 		listData.value = records
 		count.value = total
-
+	
 	}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 	$color: rgb(255, 231, 17);
-	$box-color: rgb(249, 249, 249);
-	$width:30rpx;
-	$half-width: 15rpx;
 
-	.box {
-		height: 100%;
-
-		.content {
-			height: calc(100% - 300rpx)
-		}
-	}
-
-	.header {
-		height: 100px;
-		padding: 100rpx 30rpx 0;
-
-		.header-search {
-			height: 60rpx;
-			border-radius: 30rpx;
-			border: 2px solid $color;
-			display: flex;
-			align-items: center;
-
-			.header-search__icon {
-				margin-left: 20rpx;
-				margin-right: 20rpx;
-				width: 40rpx;
-				height: 40rpx;
-			}
-
-			.header-search__line {
-				margin-right: 20rpx;
-				width: 4rpx;
-				height: 40rpx;
-				background-color: #eee;
-			}
-
-			.header-search__text {
-				flex: 1;
-			}
-
-			.header-search__btn {
-				height: 60rpx;
-				line-height: 60rpx;
-				background-color: $color;
-				padding: 0 30rpx;
-				border-radius: 30rpx;
-			}
-		}
-	}
-
-	.activity {
-		padding: $width;
+	.page-container {
+		padding-top: 120px;
+		width: 100%;
 		display: flex;
-		.activity__wrap {
-			width: 50%;
-			height: 400rpx;
-			border-radius: 10rpx;
-			// background-color: $box-color;
-			box-sizing: border-box;
-			&:nth-child(odd) {
-				padding-right: $half-width;
-			}
-			
-			&:nth-child(even) {
-				padding-left: $half-width;
-			}
-			.activity__item{
-				height: 100%;
-				background-color: $box-color;
-			}
-		}
-	}
+		justify-content: center;
 
-	.type {
-		position: sticky;
-		top: 0;
-		z-index: 10;
-	}
+		.main-wrapper {
+			width: 100%;
+			position: relative;
+			// padding-top: 20px;
 
-	.container {
-		display: flex;
-		flex-wrap: wrap;
-		padding: $width;
-
-		.item-wrapper {
-			width: 50%;
-			margin-bottom: $width;
-			box-sizing: border-box;
-
-			&:nth-child(odd) {
-				padding-right: $half-width;
-			}
-
-			&:nth-child(even) {
-				padding-left: $half-width;
-			}
-
-			.item {
+			.topbar-wrapper {
+				position: fixed;
 				width: 100%;
+				height: 120px;
+				top: 0;
+				left: 0;
+				z-index: 101;
+				box-sizing: border-box;
+				padding: 20px;
+				background: #fff;
+
+				.header-search {
+					margin-top: 60px;
+					height: 60rpx;
+					border-radius: 30rpx;
+					border: 2px solid $color;
+					display: flex;
+					align-items: center;
+
+					.header-search__icon {
+						margin-left: 20rpx;
+						margin-right: 20rpx;
+						width: 40rpx;
+						height: 40rpx;
+					}
+
+					.header-search__line {
+						margin-right: 20rpx;
+						width: 4rpx;
+						height: 40rpx;
+						background-color: #eee;
+					}
+
+					.header-search__text {
+						flex: 1;
+					}
+
+					.header-search__btn {
+						height: 62rpx;
+						margin-right: -1px;
+						line-height: 62rpx;
+						background-color: $color;
+						padding: 0 30rpx;
+						border-radius: 30rpx;
+					}
+				}
+
+			}
+
+			.gonge-wrapper {
+				position: relative;
+				width: 670rpx;
+				height: 300rpx;
+				margin: 12rpx 40rpx 0 40rpx;
+				border-radius: 24rpx;
+				background-color: #eee;
+				flex-wrap: wrap;
+				overflow: hidden;
+			}
+
+			.category-wrapper {
+				padding: 40rpx;
+				position: sticky;
+				top: 120px;
+				z-index: 10;
+				background: #fff;
+				display: flex;
+
+				.category-item {
+					background-color: $color;
+					width: 160rpx;
+					padding: 10rpx 20rpx;
+					border-radius: 40rpx;
+					margin-right: 10rpx;
+					text-align: center;
+				}
+			}
+
+			.recommend-container {
+				display: flex;
+				position: relative;
+				box-sizing: border-box;
+				overflow: hidden;
+				flex-wrap: wrap;
+				padding: 0 20rpx;
+
+				.recommend-item {
+					position: relative;
+					width: 50%;
+					box-sizing: border-box;
+					display: flex;
+					flex-direction: column;
+					background: #fff;
+					padding: 0 20rpx 40rpx;
+
+					.recommend-img-wrapper {
+						height: 340rpx;
+						align-items: center;
+						justify-content: center;
+
+						.recommend-img {
+							width: 100%;
+							height: 100%;
+						}
+					}
+
+					.recommend-info {
+						text-decoration: none;
+
+						.recommend-title {
+							margin-top: 24rpx;
+							height: 76rpx;
+							position: relative;
+							line-height: 38rpx;
+							overflow: hidden;
+							text-overflow: ellipsis;
+							min-height: 0;
+
+							.recommend-title-p {
+								font-size: 26rpx;
+								color: #333;
+							}
+						}
+
+						.recommend-price-box {
+							margin-top: 22rpx;
+							height: 40rpx;
+							line-height: 40rpx;
+
+							.recommend-sign {
+								font-size: 24rpx;
+								color: #ff5500
+							}
+
+							.recommend-price {
+								font-size: 28rpx;
+								line-height: 32rpx;
+								color: #ff5500
+							}
+
+							.recommend-payed {
+								font-size: 24rpx;
+								color: #999;
+								margin-top: 4rpx;
+							}
+						}
+
+						.recommend-person-box {
+							display: flex;
+							align-items: center;
+							margin-top: 20rpx;
+							.recommend-avatar{
+								width: 40rpx;
+								height: 40rpx;
+								border-radius: 20rpx;
+							}
+							.recommend-address{
+								flex: 1;
+								font-size: 28rpx;
+								margin-left: 10rpx;
+								color: rgb(168,168,168);
+							}
+							.recommend-credit{
+								background:rgb(244,244,252);
+								color: blue;
+								border-radius: 40rpx;
+								padding: 10rpx 20rpx;
+								font-size: 24rpx;
+							}
+						}
+					}
+				}
 			}
 		}
 	}
